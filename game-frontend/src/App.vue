6b305cd52baf8a57c5d3dfa484e1554e7dcdf7e0
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-08-30 20:59:35 -0400

    Add fetch

--- game-frontend/src/App.vue
+++ game-frontend/src/App.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+async function getData() {
+  const url = "http://localhost:8080";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +21,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <button @click="getData"></button>
 </template>
 
 <style scoped></style>
