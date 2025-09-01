d2301f164941e9d083d37cb6a094a7b8ce69123d
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:23 -0400

    Change app view to router view

--- game-frontend/src/App.vue
+++ game-frontend/src/App.vue
@@ -1,27 +1,9 @@
 <script setup>
-async function getData() {
-  const url = "http://localhost:8080/user";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <button @click="getData"></button>
+  <RouterView></RouterView>
+
 </template>
 
 <style scoped></style>
