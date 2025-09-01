1c6e48331123e8b963bad2de32f6327c129ed471
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:35:34 -0400

    Create Player entity

---
+++ game-backend/src/main/java/com/backend/model/Player.java
@@ -0,0 +1,21 @@
+package com.backend.model;
+
+public class Player {
+    private String username;
+
+    public Player() {
+    }
+
+    public Player(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
