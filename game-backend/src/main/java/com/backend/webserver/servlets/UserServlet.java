70e50fe00893e77de81f37cc4f23196e29bfe8e7
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:11 -0400

    New servlet

---
+++ game-backend/src/main/java/com/backend/webserver/servlets/UserServlet.java
@@ -0,0 +1,44 @@
+package com.backend.webserver.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.backend.model.Player;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserServlet extends HttpServlet {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private String corsHeader = "Access-Control-Allow-Origin";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader(this.corsHeader, req.getHeader("origin"));
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter response = resp.getWriter();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader(this.corsHeader, req.getHeader("origin"));
+        resp.setContentType("application.json");
+        resp.setCharacterEncoding("UTF-8");
+
+        try {
+            Player player = objectMapper.readValue(req.getReader(), Player.class);
+            System.out.println(player.getUsername());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
