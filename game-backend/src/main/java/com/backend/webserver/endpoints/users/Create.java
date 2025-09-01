0f37d1522c71eeeb4e0e02d21d32fe47697cb92d
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:03 -0400

    New endpoint

---
+++ game-backend/src/main/java/com/backend/webserver/endpoints/users/Create.java
@@ -0,0 +1,22 @@
+package com.backend.webserver.endpoints.users;
+
+import java.io.IOException;
+
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/users/create")
+public class Create {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Socket connected: " + session);
+        try {
+            session.getBasicRemote().sendText("HELLO");
+        } catch (IOException e) {
+
+        }
+    }
+
+}
