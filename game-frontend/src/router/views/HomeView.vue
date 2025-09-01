90a36eb607d4f3c1805262d7e6a9050d85bd7723
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:35 -0400

    Create home view

---
+++ game-frontend/src/router/views/HomeView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+
+const router = useRouter()
+
+const username = ref('');
+
+const goToLobby = () => {
+  router.push("/lobby")
+  sendUsername()
+}
+
+async function sendUsername() {
+  const url = "http://localhost:8080/user";
+  console.log(username.value)
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({username: username.value}),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+
+</script>
+
+<template>
+
+  <div class="fullscreen">
+    <input type="text" v-model="username" placeholder="Enter a nickname!">
+    <button type="submit" value="Start" @click="goToLobby">Start</button>
+  </div>
+</template>
+
+<style scoped>
+.fullscreen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+
+}
+</style>
