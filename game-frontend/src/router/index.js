096642261bfb6b9e17a20fe0abd277a20a0ccd28
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:29 -0400

    Add more routes

--- game-frontend/src/router/index.js
+++ game-frontend/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+import LobbyView from './views/LobbyView.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  {path: '/lobby', component: LobbyView}
+
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
