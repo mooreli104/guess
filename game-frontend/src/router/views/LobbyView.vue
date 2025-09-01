c21c5095a7d4287fe591b1acbf6204207ffc4bba
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-01 18:36:40 -0400

    create lobby view

---
+++ game-frontend/src/router/views/LobbyView.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import { useRouter } from 'vue-router'
+
+
+const router = useRouter() // Uses router from main.js to push Lobby page
+
+
+const goToHome = () => {
+  router.push('/')
+}
+
+
+</script>
+
+<template>
+    <div class = "fullscreen">
+    <button @click="goToHome">HOME</button>
+    </div>
+</template>
+
+<style scoped>
+
+.fullscreen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+  
+}
+
+
+</style>
