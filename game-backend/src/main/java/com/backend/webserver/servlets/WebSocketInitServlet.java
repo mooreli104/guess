ba564d5c2263f6bf60ceb026f989183dc5abd0f0
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-04 16:50:30 -0400

    Create websocket initalization

---
+++ game-backend/src/main/java/com/backend/webserver/servlets/WebSocketInitServlet.java
@@ -0,0 +1,28 @@
+package com.backend.webserver.servlets;
+
+import com.backend.webserver.endpoints.users.CreateUser;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.websocket.DeploymentException;
+import jakarta.websocket.server.ServerContainer;
+
+public class WebSocketInitServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        try {
+            // Retrieve the ServerContainer from the ServletContext attributes.
+            ServerContainer container = (ServerContainer) getServletContext()
+                    .getAttribute(ServerContainer.class.getName());
+
+            // Configure the ServerContainer.
+            container.setDefaultMaxTextMessageBufferSize(128 * 1024);
+
+            // Simple registration of your WebSocket endpoints.
+            container.addEndpoint(CreateUser.class);
+
+        } catch (DeploymentException x) {
+            throw new ServletException(x);
+        }
+    }
+}
