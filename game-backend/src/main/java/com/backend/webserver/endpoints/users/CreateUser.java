ade640afc941c0020f6ccc5f6db7956c86763165
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-04 16:50:01 -0400

    Rename

---
+++ game-backend/src/main/java/com/backend/webserver/endpoints/users/CreateUser.java
@@ -0,0 +1,34 @@
+package com.backend.webserver.endpoints.users;
+
+import java.io.IOException;
+
+import jakarta.websocket.CloseReason;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/user")
+public class CreateUser {
+
+    public CreateUser() {
+    }
+
+    @OnOpen
+    public void onWebSocketConnect(Session session) {
+        System.out.println("Socket connected: " + session);
+    }
+
+    @OnMessage
+    public void onWebSocketText(Session session, String message) {
+        System.out.println("Received: " + message);
+
+    }
+
+    @OnClose
+    public void onWebSocketClose(CloseReason reason) {
+        System.out.println("Socket closed: " + reason);
+    }
+
+}
