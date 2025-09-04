91551d6dd729a8850f327bb10806553f2336241b
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-04 16:49:44 -0400

    Add UUID

--- game-backend/src/main/java/com/backend/model/Player.java
+++ game-backend/src/main/java/com/backend/model/Player.java
@@ -1,13 +1,17 @@
 package com.backend.model;
 
+import java.util.UUID;
+
 public class Player {
     private String username;
+    private UUID uuid;
 
     public Player() {
     }
 
     public Player(String username) {
         this.username = username;
+        this.uuid = UUID.randomUUID();
     }
 
     public String getUsername() {
@@ -16,6 +20,14 @@ public class Player {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public UUID getUUID() {
+        return this.uuid;
+    }
+
+    public void setUUID() {
+        this.uuid = UUID.randomUUID();
     }
 
 }
