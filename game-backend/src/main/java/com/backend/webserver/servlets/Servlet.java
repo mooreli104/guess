1c12e4a66bd338ea3a1f82be2eeb3d86b70f8f09
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-04 16:50:16 -0400

    Create new servlet

---
+++ game-backend/src/main/java/com/backend/webserver/servlets/Servlet.java
@@ -0,0 +1,52 @@
+package com.backend.webserver.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.backend.database.Database;
+import com.backend.model.Player;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private String corsHeader = "Access-Control-Allow-Origin";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader(this.corsHeader, req.getHeader("origin"));
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        String jsonString = "{\"name\":\"John Doe\"}";
+
+        PrintWriter response = resp.getWriter();
+        response.println(jsonString);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader(this.corsHeader, req.getHeader("origin"));
+        resp.setContentType("application.json");
+        resp.setCharacterEncoding("UTF-8");
+
+        try {
+            Player player = objectMapper.readValue(req.getReader(), Player.class);
+            System.out.println(player.getUsername());
+            player.setUUID();
+
+            Database db = new Database();
+            System.out.println(db.getCatalog());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
