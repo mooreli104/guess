4acc291b6fd68d7eaec6c99cbc3e8c39631eac41
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-06 21:44:40 -0400

    Add attributes

--- game-backend/src/main/java/com/backend/model/Lobby.java
+++ game-backend/src/main/java/com/backend/model/Lobby.java
@@ -1,5 +1,27 @@
 package com.backend.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Lobby")
+
 public class Lobby {
+    @Id
+    private UUID lobbyID;
+
+    public Lobby() {
+    }
+
+    public void setLobbyID() {
+        this.lobbyID = UUID.randomUUID();
+    }
+
+    public UUID getLobbyID() {
+        return this.lobbyID;
+    }
 
 }
