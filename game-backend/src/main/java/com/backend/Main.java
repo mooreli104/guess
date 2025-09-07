43fc8497181814d3ec0fbc48af18723ff75af9bd
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-07 16:36:31 -0400

    Refactor

--- game-backend/src/main/java/com/backend/Main.java
+++ game-backend/src/main/java/com/backend/Main.java
@@ -4,18 +4,14 @@ import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee11.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
 import org.eclipse.jetty.server.Server;
 
-import com.backend.database.Database;
 import com.backend.webserver.servlets.Servlet;
 import com.backend.webserver.servlets.WebSocketInitServlet;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Create a Server with a ServerConnector listening on port 8080.
         Server server = new Server(8080);
-        Database database = new Database();
 
-        // Create a ServletContextHandler with the given context path.
         ServletContextHandler handler = new ServletContextHandler("/");
         server.setHandler(handler);
 
@@ -23,11 +19,9 @@ public class Main {
         // to setup the ServerContainer for this web application context.
         JakartaWebSocketServletContainerInitializer.configure(handler, null);
 
-        // Add a WebSocket-initializer Servlet to register WebSocket endpoints.
         handler.addServlet(WebSocketInitServlet.class, "/ws");
         handler.addServlet(Servlet.class, "/api");
 
-        // Starting the Server will start the ServletContextHandler.
         try {
             server.start();
         } catch (Exception e) {
