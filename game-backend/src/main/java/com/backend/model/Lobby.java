a50cb11142857453098ad54ef4c0fc52d8789970
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-07 16:36:49 -0400

    Small changes

--- game-backend/src/main/java/com/backend/model/Lobby.java
+++ game-backend/src/main/java/com/backend/model/Lobby.java
@@ -1,5 +1,6 @@
 package com.backend.model;
 
+import java.util.Set;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
@@ -8,12 +9,15 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Lobby")
-
 public class Lobby {
     @Id
     private UUID lobbyID;
 
     public Lobby() {
+    }
+
+    public Lobby(UUID uuid) {
+        this.lobbyID = uuid;
     }
 
     public void setLobbyID() {
@@ -22,6 +26,11 @@ public class Lobby {
 
     public UUID getLobbyID() {
         return this.lobbyID;
+    }
+
+    @Override
+    public String toString() {
+        return this.lobbyID.toString();
     }
 
 }
