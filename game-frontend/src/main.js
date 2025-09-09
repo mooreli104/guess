c817498c44cac71aaa1385a2b547c91af3555482
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-08 21:31:13 -0400

    Attatch primevue to app

--- game-frontend/src/main.js
+++ game-frontend/src/main.js
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Nora from '@primeuix/themes/nora'
+
+
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+    theme:
+    {
+        preset: Nora
+    }
+    
+})
 
 app.mount('#app')
