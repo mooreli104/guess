4bbae571d5d5f0ed92cc043505eb3abc4412f584
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-11 16:32:13 -0400

    Change directory

---
+++ game-backend/src/main/java/com/backend/util/HibernateUtil.java
@@ -0,0 +1,52 @@
+package com.backend.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernatePersistenceConfiguration;
+import org.hibernate.tool.schema.Action;
+
+import com.backend.model.Lobby;
+import com.backend.model.Player;
+
+public class HibernateUtil {
+
+    private static HibernateUtil hibernate;
+    private static SessionFactory sessionFactory;
+    private static final String USER = "moore";
+    private static final String PASSWORD = "sachisroom";
+
+    private HibernateUtil() {
+        try {
+            HibernateUtil.sessionFactory = new HibernatePersistenceConfiguration("lobby")
+                    .managedClass(Lobby.class)
+                    .managedClass(Player.class)
+                    // PostgreSQL
+                    .jdbcUrl("jdbc:postgresql://localhost/gameDB")
+                    // Credentials
+                    .jdbcCredentials(USER, PASSWORD)
+                    // Automatic schema export
+                    .schemaToolingAction(Action.CREATE)
+                    // SQL statement logging
+                    .showSql(true, true, true)
+                    // Create a new SessionFactory
+                    .createEntityManagerFactory();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static HibernateUtil getInstance() {
+        if (HibernateUtil.hibernate == null) {
+            hibernate = new HibernateUtil();
+        }
+        return hibernate;
+    }
+
+    public void persist(Object e) {
+        sessionFactory.inTransaction(session -> {
+            session.persist(e);
+            session.flush();
+        });
+    }
+
+}
