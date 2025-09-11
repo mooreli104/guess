827a4e1b2f1f8adc9f1c982291392a046e615e8b
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-11 16:32:23 -0400

    Get players

--- game-frontend/src/router/views/LobbyView.vue
+++ game-frontend/src/router/views/LobbyView.vue
@@ -11,6 +11,25 @@ const goToHome = () => {
   router.push('/')
 }
 
+
+async function getPlayers() {
+  const url = "http://localhost:8080/api";
+  console.log(username.value)
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 </script>
 
 <template>
