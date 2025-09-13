ef94eb15a1e2b18156b25be8b293450b236e8f5b
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-12 20:43:47 -0400

    Create singleton pattern

--- game-backend/src/main/java/com/backend/persistence/LobbyDao.java
+++ game-backend/src/main/java/com/backend/persistence/LobbyDao.java
@@ -1,5 +1,25 @@
 package com.backend.persistence;
 
+import com.backend.model.Lobby;
+import com.backend.util.HibernateUtil;
+
 public class LobbyDao {
+    private static LobbyDao lobbyDao;
+    private HibernateUtil hibernate;
+
+    private LobbyDao() {
+        this.hibernate = HibernateUtil.getInstance();
+    }
+
+    public static LobbyDao getInstance() {
+        if (LobbyDao.lobbyDao == null) {
+            LobbyDao.lobbyDao = new LobbyDao();
+        }
+        return LobbyDao.lobbyDao;
+    }
+
+    public void registerLobby(Lobby lobby) {
+        this.hibernate.persist(lobby);
+    }
 
 }
