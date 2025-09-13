b39354c69cb449954a59ce0b7589e2bca1a325f7
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-12 20:43:36 -0400

    Add behavior methods

--- game-backend/src/main/java/com/backend/model/Player.java
+++ game-backend/src/main/java/com/backend/model/Player.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 public class Player {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
     private String username;
     private int score;
@@ -33,44 +36,29 @@ public class Player {
         this.guess = null;
     }
 
+    public void guess(String guess) {
+        this.guess = guess;
+    }
+
+    public void joinLobby(Lobby lobby) {
+
+        this.lobby = lobby;
+    }
+
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public UUID getUUID() {
-        return this.uuid;
-    }
-
-    public void setUUID() {
-        this.uuid = UUID.randomUUID();
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
     }
 
     public String getGuess() {
         return guess;
     }
 
-    public void setGuess(String guess) {
-        this.guess = guess;
+    public int getScore() {
+        return score;
     }
 
-    public void setLobby(Lobby lobby) {
-        this.lobby = lobby;
-    }
-
-    public Lobby getLobby() {
-        return lobby;
+    public void leaveLobby() {
+        this.lobby = null;
     }
 
 }
