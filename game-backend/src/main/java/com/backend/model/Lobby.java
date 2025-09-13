cdb7ba004b4c65e23c393841d44eb4d706af7e6b
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-12 20:43:27 -0400

    Add @GeneratedValue and addPlayer methods

--- game-backend/src/main/java/com/backend/model/Lobby.java
+++ game-backend/src/main/java/com/backend/model/Lobby.java
@@ -1,12 +1,14 @@
 package com.backend.model;
 
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.Set;
 import java.util.UUID;
 
-import jakarta.persistence.ElementCollection;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -15,24 +17,25 @@ import jakarta.persistence.Table;
 @Table(name = "Lobby")
 public class Lobby {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID lobbyID;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "lobby")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "lobby", orphanRemoval = true)
     private Set<Player> players;
 
     public Lobby() {
     }
 
-    public Lobby(UUID uuid) {
-        this.lobbyID = uuid;
-    }
-
-    public void setLobbyID() {
-        this.lobbyID = UUID.randomUUID();
-    }
-
     public UUID getLobbyID() {
         return this.lobbyID;
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public void addPlayer(Collection<Player> players) {
+        this.players.addAll(players);
     }
 
     @Override
