7e214e9cd44dafc87390de0a711b13b89a017ccf
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-12 20:43:53 -0400

    Create singleton pattern

--- game-backend/src/main/java/com/backend/persistence/UserDao.java
+++ game-backend/src/main/java/com/backend/persistence/UserDao.java
@@ -1,5 +1,25 @@
 package com.backend.persistence;
 
+import com.backend.model.Player;
+import com.backend.util.HibernateUtil;
+
 public class UserDao {
+    private static UserDao userDao;
+    private HibernateUtil hibernate;
+
+    private UserDao() {
+        this.hibernate = HibernateUtil.getInstance();
+    }
+
+    public static UserDao getInstance() {
+        if (UserDao.userDao == null) {
+            UserDao.userDao = new UserDao();
+        }
+        return UserDao.userDao;
+    }
+
+    public void registerPlayer(Player player) {
+        this.hibernate.persist(player);
+    }
 
 }
