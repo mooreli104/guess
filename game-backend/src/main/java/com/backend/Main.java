0253c6d2d38302fd39dc79224738dc5b4ed964cc
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-12 20:42:45 -0400

    Add CORS handler

--- game-backend/src/main/java/com/backend/Main.java
+++ game-backend/src/main/java/com/backend/Main.java
@@ -1,10 +1,14 @@
 package com.backend;
 
+import java.util.Set;
+
 import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee11.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.CrossOriginHandler;
 
-import com.backend.controller.servlets.Servlet;
+import com.backend.controller.servlets.GameServlet;
 import com.backend.controller.servlets.WebSocketInitServlet;
 
 public class Main {
@@ -12,15 +16,22 @@ public class Main {
 
         Server server = new Server(8080);
 
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+
         ServletContextHandler handler = new ServletContextHandler("/");
-        server.setHandler(handler);
+        CrossOriginHandler crossOriginHandler = new CrossOriginHandler();
+
+        crossOriginHandler.setAllowedOriginPatterns(Set.of("http://localhost:5173"));
+
+        contexts.setHandlers(crossOriginHandler, handler);
+        server.setHandler(contexts);
 
         // Ensure that JavaxWebSocketServletContainerInitializer is initialized,
         // to setup the ServerContainer for this web application context.
         JakartaWebSocketServletContainerInitializer.configure(handler, null);
 
         handler.addServlet(WebSocketInitServlet.class, "/ws");
-        handler.addServlet(Servlet.class, "/api");
+        handler.addServlet(GameServlet.class, "/api");
 
         try {
             server.start();
