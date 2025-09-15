b3b3d34561107729167cf17a7a49fe7660e1aa23
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-14 22:08:02 -0400

    Add persistence logic

--- game-backend/src/main/java/com/backend/persistence/LobbyDao.java
+++ game-backend/src/main/java/com/backend/persistence/LobbyDao.java
@@ -3,12 +3,11 @@ package com.backend.persistence;
 import com.backend.model.Lobby;
 import com.backend.util.HibernateUtil;
 
-public class LobbyDao {
+public class LobbyDao implements BaseDao<Lobby> {
     private static LobbyDao lobbyDao;
-    private HibernateUtil hibernate;
 
     private LobbyDao() {
-        this.hibernate = HibernateUtil.getInstance();
+        LobbyDao.lobbyDao = new LobbyDao();
     }
 
     public static LobbyDao getInstance() {
@@ -18,8 +17,26 @@ public class LobbyDao {
         return LobbyDao.lobbyDao;
     }
 
-    public void registerLobby(Lobby lobby) {
-        this.hibernate.persist(lobby);
+    @Override
+    public void save(Lobby lobby) {
+        HibernateUtil.getInstance().inTransaction(session -> {
+            session.persist(lobby);
+        });
+    }
+
+    @Override
+    public Lobby findById(String id) {
+        Lobby lobby = HibernateUtil.getInstance().fromTransaction(session -> session.byNaturalId(Lobby.class)
+                .using("session", id)
+                .load());
+        return lobby;
+    }
+
+    @Override
+    public void update() {
+        HibernateUtil.getInstance().inTransaction(session -> {
+            session.flush();
+        });
     }
 
 }
