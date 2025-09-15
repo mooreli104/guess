4b0541992c6812db35678f8f5296d5822acfe32b
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-14 22:08:54 -0400

    Create component

---
+++ game-frontend/src/router/components/Players.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import Player from 'Player.vue';
+const props = defineProps(['players'])
+
+
+</script>
+
+<template>
+
+    <Player v-for="player in props.players" :username  = "player.username"></Player>
+
+
+</template>
+
+<style scoped>
+
+
+
+</style>
