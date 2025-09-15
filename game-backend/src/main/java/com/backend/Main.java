68797a6afe24b5a99142cb062b40b8abb54e50da
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-14 22:06:29 -0400

    Add socket endpoint into main

--- game-backend/src/main/java/com/backend/Main.java
+++ game-backend/src/main/java/com/backend/Main.java
@@ -1,15 +1,15 @@
 package com.backend;
 
 import java.util.Set;
-
 import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee11.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
+import org.eclipse.jetty.server.Context;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.CrossOriginHandler;
 
+import com.backend.controller.endpoints.GameEndpoint;
 import com.backend.controller.servlets.GameServlet;
-import com.backend.controller.servlets.WebSocketInitServlet;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,26 +18,25 @@ public class Main {
 
         ContextHandlerCollection contexts = new ContextHandlerCollection();
 
-        ServletContextHandler handler = new ServletContextHandler("/");
+        ServletContextHandler socketHandler = new ServletContextHandler("/ws/*");
+        socketHandler.addServlet(GameServlet.class, "/api/*");
+
         CrossOriginHandler crossOriginHandler = new CrossOriginHandler();
-
         crossOriginHandler.setAllowedOriginPatterns(Set.of("http://localhost:5173"));
-
-        contexts.setHandlers(crossOriginHandler, handler);
+        contexts.setHandlers(crossOriginHandler, socketHandler);
         server.setHandler(contexts);
 
         // Ensure that JavaxWebSocketServletContainerInitializer is initialized,
         // to setup the ServerContainer for this web application context.
-        JakartaWebSocketServletContainerInitializer.configure(handler, null);
-
-        handler.addServlet(WebSocketInitServlet.class, "/ws");
-        handler.addServlet(GameServlet.class, "/api");
+        JakartaWebSocketServletContainerInitializer.configure(socketHandler, (servletContext, container) -> {
+            container.setDefaultMaxBinaryMessageBufferSize(128 * 1024);
+            container.addEndpoint(GameEndpoint.class);
+        });
 
         try {
             server.start();
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 }
