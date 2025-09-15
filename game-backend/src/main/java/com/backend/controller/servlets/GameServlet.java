1c0212389ca1615d13d6cf1b6701185f005da80f
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-14 22:07:19 -0400

    Move business logic into service layer

--- game-backend/src/main/java/com/backend/controller/servlets/GameServlet.java
+++ game-backend/src/main/java/com/backend/controller/servlets/GameServlet.java
@@ -2,13 +2,6 @@ package com.backend.controller.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Reader;
-
-import com.backend.model.Lobby;
-import com.backend.model.Player;
-import com.backend.service.GameService;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -18,8 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GameServlet extends HttpServlet {
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private String corsHeader = "Access-Control-Allow-Origin";
     private Logger logger = LoggerFactory.getLogger(GameServlet.class);
@@ -32,32 +23,7 @@ public class GameServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setHeader(this.corsHeader, req.getHeader("origin"));
-        String jsonString = "{\"name\":\"John Doe\"}";
 
-        switch (req.getContextPath()) {
-            case "/connect":
-                Reader player = req.getReader();
-                connectToLobby(player);
-                sendResponse(resp, jsonString);
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
-    public void connectToLobby(Reader input) {
-        try {
-            Player player = objectMapper.readValue(input, Player.class);
-            Lobby lobby = new Lobby();
-
-            GameService gameService = GameService.getInstance();
-            gameService.createLobby(lobby, player);
-
-        } catch (Exception e) {
-            this.logger.error("Connection error", e);
-        }
     }
 
     public void sendResponse(HttpServletResponse response, String message) {
