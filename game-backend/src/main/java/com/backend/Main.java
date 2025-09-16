ccf2335579904a381ecb7f10b75589219701a7e3
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-15 22:45:47 -0400

    Move around code

--- game-backend/src/main/java/com/backend/Main.java
+++ game-backend/src/main/java/com/backend/Main.java
@@ -3,9 +3,7 @@ package com.backend;
 import java.util.Set;
 import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee11.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
-import org.eclipse.jetty.server.Context;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.CrossOriginHandler;
 
 import com.backend.controller.endpoints.GameEndpoint;
@@ -14,26 +12,27 @@ import com.backend.controller.servlets.GameServlet;
 public class Main {
     public static void main(String[] args) {
 
-        Server server = new Server(8080);
-
-        ContextHandlerCollection contexts = new ContextHandlerCollection();
-
-        ServletContextHandler socketHandler = new ServletContextHandler("/ws/*");
-        socketHandler.addServlet(GameServlet.class, "/api/*");
-
-        CrossOriginHandler crossOriginHandler = new CrossOriginHandler();
-        crossOriginHandler.setAllowedOriginPatterns(Set.of("http://localhost:5173"));
-        contexts.setHandlers(crossOriginHandler, socketHandler);
-        server.setHandler(contexts);
-
-        // Ensure that JavaxWebSocketServletContainerInitializer is initialized,
-        // to setup the ServerContainer for this web application context.
-        JakartaWebSocketServletContainerInitializer.configure(socketHandler, (servletContext, container) -> {
-            container.setDefaultMaxBinaryMessageBufferSize(128 * 1024);
-            container.addEndpoint(GameEndpoint.class);
-        });
-
         try {
+            Server server = new Server(8080);
+
+            ServletContextHandler socketHandler = new ServletContextHandler("/ws");
+
+            // Ensure that JavaxWebSocketServletContainerInitializer is initialized,
+            // to setup the ServerContainer for this web application context.
+            JakartaWebSocketServletContainerInitializer.configure(socketHandler, (servletContext, container) -> {
+                container.setDefaultMaxBinaryMessageBufferSize(128 * 1024);
+                container.addEndpoint(GameEndpoint.class);
+
+            });
+
+            CrossOriginHandler crossOriginHandler = new CrossOriginHandler();
+            crossOriginHandler.setAllowedOriginPatterns(Set.of("http://localhost:5173"));
+            server.setHandler(crossOriginHandler);
+
+            crossOriginHandler.setHandler(socketHandler);
+
+            socketHandler.addServlet(GameServlet.class, "/api/*");
+
             server.start();
         } catch (Exception e) {
             System.out.println(e);
