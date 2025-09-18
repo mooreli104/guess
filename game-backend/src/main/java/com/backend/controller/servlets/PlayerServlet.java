e305e3969349736a713bba304e13f15ab23e7a58
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-18 19:00:13 -0400

    New servlet

---
+++ game-backend/src/main/java/com/backend/controller/servlets/PlayerServlet.java
@@ -0,0 +1,41 @@
+package com.backend.controller.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlayerServlet extends HttpServlet {
+
+    private String corsHeader = "Access-Control-Allow-Origin";
+    private Logger logger = LoggerFactory.getLogger(PlayerServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader(this.corsHeader, req.getHeader("origin"));
+
+    }
+
+    public void sendResponse(HttpServletResponse response, String message) {
+
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.println(message);
+
+        } catch (Exception e) {
+            this.logger.error("Response error", e);
+        }
+
+    }
+
+}
