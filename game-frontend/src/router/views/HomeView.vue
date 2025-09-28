b3af5d7bb822a7718f31a25961f48609334a1b2f
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-28 18:34:49 -0400

    Add new button

--- game-frontend/src/router/views/HomeView.vue
+++ game-frontend/src/router/views/HomeView.vue
@@ -19,19 +19,40 @@ async function createLobby() {
 }
 
 
+
 </script>
 
 <template>
-
   <div class="fullscreen">
     <input type="text" v-model="username" placeholder="Enter a nickname!">
-    <Button type="submit" value="Start" @click.prevent="goToLobby">Start</Button>
+    <Button v-if="username" type="submit" value="Start" @click.prevent="goToLobby" label="normal" raised rounded style = "top: 1%">Start</Button>
+    <Button v-else label="normal" severity="secondary" raised rounded >Start</Button>
   </div>
 </template>
 
 <style scoped>
+
+
+*{
+  box-sizing: border-box;
+}
+
+body{
+	font-family: 'Playfair Display', serif;
+	color: rgba(172,170,190, 1);
+	text-rendering: optimizeLegibility;
+	font-smooth: always;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: purple;
+	font-size: 14px;
+	overflow: hidden;
+}
+
+
 .fullscreen {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   position: fixed;
@@ -40,6 +61,6 @@ async function createLobby() {
   bottom: 0;
   right: 0;
   overflow: auto;
-
 }
+
 </style>
