fadc279191689a7ed70322b7a4f59de604d410e4
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-28 18:13:45 -0400

    Add props to recieve player

--- game-frontend/src/router/components/Player.vue
+++ game-frontend/src/router/components/Player.vue
@@ -1,14 +1,15 @@
 <script setup>
 
 import Avatar from 'primevue/avatar';
-
+const props = defineProps(['player'])
+defineOptions({
+  inheritAttrs: false
+})
 
 </script>
 
 <template>
-
-<Avatar></Avatar>
-
+<Avatar>{{ props.player[0]["username"]}}</Avatar>
 </template>
 
 <style scoped>
