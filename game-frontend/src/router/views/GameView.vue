6c00978dfedd9eee5eaeabdb2ffc54f31a27efde
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-09-28 18:34:56 -0400

    Create GameView

---
+++ game-frontend/src/router/views/GameView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { useRouter } from 'vue-router'
+
+import { useSocket } from '@/stores/store';
+
+const router = useRouter() // Uses router from main.js to push Lobby page
+const socket = useSocket()
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+
+
+</style>
