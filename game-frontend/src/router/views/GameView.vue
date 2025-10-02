3ab4e5c289e4b332ec5df29560097a23dd7487c3
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-10-01 23:32:34 -0400

    Add fetch image function

--- game-frontend/src/router/views/GameView.vue
+++ game-frontend/src/router/views/GameView.vue
@@ -7,6 +7,24 @@ import { useSocket } from '@/stores/store';
 const router = useRouter() // Uses router from main.js to push Lobby page
 const socket = useSocket()
 
+async function getImage() {
+  const url = "http://localhost:8080/game";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+//getImage()
+
+
 </script>
 
 <template>
