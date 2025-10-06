ed851e5e2eaa781fb9ad18bc85d7f1e5675dea8a
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-10-05 23:21:40 -0400

    Create player view

--- game-frontend/src/router/views/GameView.vue
+++ game-frontend/src/router/views/GameView.vue
@@ -4,10 +4,15 @@ import { useRouter } from 'vue-router'
 
 import { useSocket } from '@/stores/store';
 import { ref } from 'vue';
+import Player from '../components/Player.vue';
+import { Button } from 'primevue';
 
 const router = useRouter() // Uses router from main.js to push Lobby page
 const socket = useSocket()
 var url = ref()
+var players = ref()
+var playerGuess = ref()
+
 
 async function getImage() {
   const url = "http://localhost:8080/game";
@@ -23,43 +28,73 @@ async function getImage() {
     console.error(error.message);
   }
 }
-getImage().then((response)=>{
+getImage().then((response) => {
   url.value = (response.data[0]['node']['main_picture']['large']);
 });
+
+async function getPlayers() {
+  socket.socket.send(JSON.stringify({ "action": "getLobby" }));
+}
+socket.socket.addEventListener("message", (event) => {
+  players.value = JSON.parse(event.data)
+});
+
+getPlayers()
+
+async function guess() {
+  socket.socket.send(JSON.stringify({
+    "guess": playerGuess.value,
+    "action": "guess"
+  }));
+}
 
 </script>
 
 <template>
 
-    <div class='page'>
-    <img :src="url"></img>
+  <div class='page'>
+    <div class="players">
+      <Player v-if="players" :player=players></Player>
     </div>
+    <div class = "guess">
+    <img :src="url"></img>
+      <div class='input'>
+    <input type="text" v-model="playerGuess" placeholder="Guess">
+    <Button type="submit" value="Guess" @click.prevent="guess" label="normal" raised rounded style="top: 1%">Guess</Button>
+      </div>
+      </div>
+  </div>
 </template>
 
 <style scoped>
-
-
 .page {
   font-family: 'Times New Roman', Times, serif;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   height: 100vh;
   width: 100vw;
   background-image: linear-gradient(45deg, #E7BD8A 50%, #5B000B 50%);
+}
 
-  h2 {
-    font-size: 6.5vw;
-      position: fixed;
+img {
+    width:75%; /* you can use % */
+    height: auto;
+}
 
-    background: linear-gradient(45deg,
-        #5B000B 50%,
-        #E7BD8A 50%);
-    -webkit-text-fill-color: transparent;
-    background-clip: text;
+.guess{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5%;
+}
 
-  }
+.input{
+  display: flex;
+  margin-top: 5%;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
 
